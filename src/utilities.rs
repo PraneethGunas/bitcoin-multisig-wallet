629d7b24f5f40601c58d80233da81df7359006bd
PraneethGunas <praneethgunas404@gmail.com>
2025-04-30 12:05:59 -0400

    Enhance random xpub and mnemonic generation to include public key; add network parsing utility

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,13 +1,27 @@
+use anyhow::{Result, anyhow};
 use bitcoin::{Network, bip32::ExtendedPubKey};
 use bitcoin::secp256k1::{rand::{self, RngCore}, Secp256k1};
 use bitcoin::bip32::ExtendedPrivKey;
 use bip39::Mnemonic;
-pub fn generate_random_xpub_and_mnemonic(network: Network) -> (ExtendedPubKey, String) {
+use secp256k1::{PublicKey, SecretKey};
+pub fn generate_random_xpub_and_mnemonic(network: Network) -> (ExtendedPubKey, String, PublicKey) {
     let secp = Secp256k1::new();
     let mut seed = [0u8; 32];
     rand::thread_rng().fill_bytes(&mut seed);
     let xprv = ExtendedPrivKey::new_master(network, &seed).unwrap();
     let xpub = ExtendedPubKey::from_priv(&secp, &xprv);
+    let secret_key = SecretKey::from_slice(&seed).unwrap();
+    let public_key = PublicKey::from_secret_key(&secp, &secret_key);
     let mnemonic = Mnemonic::from_entropy(&seed).unwrap().to_string();
-    (xpub, mnemonic)
+    (xpub, mnemonic, public_key)
+}
+
+pub fn get_network_from_string(network: &str) -> Result<Network> {
+    match network.to_lowercase().as_str() {
+        "bitcoin" => Ok(Network::Bitcoin),
+        "testnet" => Ok(Network::Testnet),
+        "signet" => Ok(Network::Signet),
+        "regtest" => Ok(Network::Regtest),
+        _ => Err(anyhow!("Unsupported network: {}", network)),
+    }
 }
