47cd9e4bdab57444303f8cfde0d1e3ccde3a73bc
PraneethGunas <praneethgunas404@gmail.com>
2025-05-06 02:35:23 -0400

    Refactor key generation to use Xpub and Xpriv types; update function signature for generating random xpub and mnemonic

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,15 +1,16 @@
 use anyhow::{Result, anyhow};
-use bitcoin::{Network, bip32::ExtendedPubKey};
-use bitcoin::secp256k1::{rand::{self, RngCore}, Secp256k1};
-use bitcoin::bip32::ExtendedPrivKey;
+use bitcoin::{Network, bip32::{Xpub, Xpriv}};
+use bitcoin::secp256k1::{rand::{self, RngCore}, Secp256k1 as BitcoinSecp256k1};
 use bip39::Mnemonic;
-use secp256k1::{PublicKey, SecretKey};
-pub fn generate_random_xpub_and_mnemonic(network: Network) -> (ExtendedPubKey, String, PublicKey) {
+use secp256k1::{PublicKey, SecretKey, Secp256k1};
+
+pub fn generate_random_xpub_and_mnemonic(network: Network) -> (Xpub, String, PublicKey) {
     let secp = Secp256k1::new();
+    let b_secp = BitcoinSecp256k1::new();
     let mut seed = [0u8; 32];
     rand::thread_rng().fill_bytes(&mut seed);
-    let xprv = ExtendedPrivKey::new_master(network, &seed).unwrap();
-    let xpub = ExtendedPubKey::from_priv(&secp, &xprv);
+    let xprv = Xpriv::new_master(network, &seed).unwrap();
+    let xpub = Xpub::from_priv(&b_secp, &xprv);
     let secret_key = SecretKey::from_slice(&seed).unwrap();
     let public_key = PublicKey::from_secret_key(&secp, &secret_key);
     let mnemonic = Mnemonic::from_entropy(&seed).unwrap().to_string();
